3e7f226d9a31a0f78ad3b51db4ba74f7287bd9b9
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-04-05 18:56:16 +0300

    feat: instantly generate next execution time on creation of schedule

--- src/main/kotlin/online/danielstefani/paddy/schedule/ScheduleService.kt
+++ src/main/kotlin/online/danielstefani/paddy/schedule/ScheduleService.kt
@@ -1,18 +1,38 @@
 package online.danielstefani.paddy.schedule
 
+import com.cronutils.model.CronType
+import com.cronutils.model.definition.CronDefinitionBuilder
+import com.cronutils.model.time.ExecutionTime
+import com.cronutils.parser.CronParser
 import jakarta.enterprise.context.ApplicationScoped
 import online.danielstefani.paddy.daemon.DaemonService
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 @ApplicationScoped
 class ScheduleService(
     private val scheduleRepository: ScheduleRepository,
     private val daemonService: DaemonService
 ) {
+    companion object {
+        private val cronParser = CronParser(
+            CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ))
+    }
+
     fun createSchedule(
         daemonId: String,
         schedule: Schedule
     ): Schedule? {
         val daemon = daemonService.getDaemon(daemonId) ?: return null
+
+        if (schedule.isPeriodic()) {
+            schedule.nextExecution = ExecutionTime.forCron(cronParser.parse(schedule.periodic))
+                .nextExecution(ZonedDateTime.now(ZoneId.of(schedule.timezone)))
+                .map { it.toEpochSecond() }
+                .orElseThrow {
+                    IllegalArgumentException("Cron execution time could not be generated for cron: <${schedule.periodic}>!") }
+        }
+
         return scheduleRepository.create(daemon, schedule)
     }
 }
--- src/main/kotlin/online/danielstefani/paddy/schedule/validation/QuartzCronValidator.kt
+++ src/main/kotlin/online/danielstefani/paddy/schedule/validation/QuartzCronValidator.kt
@@ -7,14 +7,18 @@ import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
 
 class QuartzCronValidator : ConstraintValidator<QuartzCron, String> {
+    companion object {
+        private val cronParser = CronParser(
+            CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ))
+    }
+
     override fun isValid(string: String?, ctx: ConstraintValidatorContext?): Boolean {
         // This is allowed because the "single"
         // field may be populated
         if (string == null) return true
 
         try {
-            CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ))
-                .parse(string)
+            cronParser.parse(string)
             return true
         } catch (ex: IllegalArgumentException) {
             return false

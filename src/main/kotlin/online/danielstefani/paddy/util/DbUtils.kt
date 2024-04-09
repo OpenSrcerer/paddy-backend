38a8ed6fe3cc479a22fff1b53ddc9db32657a6c9
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-04-09 17:31:29 +0300

    patch: allow db utils to unwind hashmaps from DB to custom DTOs

--- src/main/kotlin/online/danielstefani/paddy/util/DbUtils.kt
+++ src/main/kotlin/online/danielstefani/paddy/util/DbUtils.kt
@@ -1,12 +1,22 @@
 package online.danielstefani.paddy.util
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.neo4j.ogm.model.Result
 
-inline fun <reified T> Result.get(): List<T> {
+val objectMapper = ObjectMapper()
+
+inline fun <reified T: Any> Result.get(): List<T> {
+
     return with(this.queryResults().iterator()) {
         if (!this.hasNext())
             emptyList<T>()
 
-        this.asSequence().toList().flatMap { it.values.map { it as T } }
+        this.asSequence().toList().flatMap<Map<String, Any>, T> { resMap ->
+            return if (resMap.size == 1)
+                resMap.values.map { it as T }
+            else
+                listOf(objectMapper.convertValue(resMap, T::class.java))
+        }
     }
+
 }

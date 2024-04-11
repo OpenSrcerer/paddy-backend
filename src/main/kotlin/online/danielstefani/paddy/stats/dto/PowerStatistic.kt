8545d8702cd330de6536f3f38af0a735e1427be7
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-04-11 17:22:23 +0300

    patch: update temporal definitions

--- src/main/kotlin/online/danielstefani/paddy/stats/dto/PowerStatistic.kt
+++ src/main/kotlin/online/danielstefani/paddy/stats/dto/PowerStatistic.kt
@@ -1,9 +1,6 @@
 package online.danielstefani.paddy.stats.dto
 
-import com.fasterxml.jackson.annotation.JsonFormat
-
 class PowerStatistic {
-    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
     var statistic: Double? = null
 
     var temporal: Long? = null
--- src/main/kotlin/online/danielstefani/paddy/stats/dto/PowerTemporal.kt
+++ src/main/kotlin/online/danielstefani/paddy/stats/dto/PowerTemporal.kt
@@ -7,24 +7,10 @@ enum class PowerTemporal(
     HOUR(MINUTE * 60),
     DAY(HOUR * 24),
     WEEK(DAY * 7),
-    FOUR_WEEKS(WEEK * 4),
-    QUARTER(FOUR_WEEKS * 3),
-    HALF_YEAR(DAY * (365 / 2)),
-    YEAR(DAY * 365);
-
-    // Returns how many hours there are in any given unit
-    fun toHours(temporal: PowerTemporal = this): Double {
-        return when (temporal) {
-            MINUTE      -> 1.0 / 60
-            HOUR        -> toHours(MINUTE) * 60
-            DAY         -> toHours(HOUR) * 24
-            WEEK        -> toHours(DAY) * 7
-            FOUR_WEEKS  -> toHours(WEEK) * 4
-            QUARTER     -> toHours(FOUR_WEEKS) * 3
-            HALF_YEAR   -> toHours(QUARTER) * 2
-            YEAR        -> toHours(HALF_YEAR) * 2
-        }
-    }
+    MONTH(2_628_000),
+    QUARTER(MONTH * 3),
+    HALF_YEAR(QUARTER * 2),
+    YEAR(HALF_YEAR * 2);
 
     operator fun times(other: Int): Int = this.valueSeconds * other
 

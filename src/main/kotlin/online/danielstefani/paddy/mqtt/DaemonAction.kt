2c55bcea624b48699396c92c5a12e5f8274e277e
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-04-13 20:33:10 +0300

    feat: 🎱 register @DaemonAction for reflection

--- src/main/kotlin/online/danielstefani/paddy/mqtt/DaemonAction.kt
+++ src/main/kotlin/online/danielstefani/paddy/mqtt/DaemonAction.kt
@@ -1,5 +1,8 @@
 package online.danielstefani.paddy.mqtt
 
+import io.quarkus.runtime.annotations.RegisterForReflection
+
+@RegisterForReflection
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
 annotation class DaemonAction(
